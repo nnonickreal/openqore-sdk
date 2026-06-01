fbd6559a42bdb33df7119cabdf400c3082ca2a44
nnonick <116197076+nnonickreal@users.noreply.github.com>
2026-06-01 06:25:28 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-if make -j "$(nproc)" T=open_source DEBUG=1 >log.txt 2>&1; then
+if make -j "$(nproc)" T=openqore_alpha DEBUG=1 ANC_APP=0 ANC_FF_ENABLED=0 ANC_FB_ENABLED=0 APP_ANC_KEY=1 SOUNDCORE_PATCH=1 >log.txt 2>&1; then
 	echo "build success"
 else
 	echo "build failed and call log.txt"
